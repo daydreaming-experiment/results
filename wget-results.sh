99662f6b3b8c45422d87a0df44e7e8037fdfd64f
Sébastien Lerique <sl@mehho.net>
2014-10-17 14:56:51 -0300

    Add script to get results from exports url

---
+++ wget-results.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+set -e
+
+if [ $# != 1 ]
+then
+  echo "Usage: $(basename $0) password"
+  exit 1
+fi
+
+echo "##"
+echo "## Getting results"
+echo "##"
+echo
+wget --http-user=results --http-password=$1 http://exports.daydreaming-the-app.net/profiles-latest.json
+wget --http-user=results --http-password=$1 http://exports.daydreaming-the-app.net/results-latest.json
